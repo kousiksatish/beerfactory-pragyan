7765cb6400a01056e1f77fd9d1ce78675f9d7dcc
sriranganathan <ravisriranganathan@gmail.com>
2015-12-17 23:29:32 +0530

    Added .env file compatability

--- beerf/manage.py
+++ beerf/manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beerf.settings")
