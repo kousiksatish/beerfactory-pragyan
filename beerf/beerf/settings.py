7cc5576d0256e1f0bd94c7d1ed4d1ea7202b6a90
anugrahaa <vr.anugrahaa@gmail.com>
2015-12-19 15:36:35 +0530

    master

--- beerf/beerf/settings.py
+++ beerf/beerf/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'beerf_15',
