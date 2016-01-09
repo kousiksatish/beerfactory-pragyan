9b98899c7936522d1ed8db646ff8f069e7081393
Anirudh58 <anirudh.jan97@gmail.com>
2016-01-09 16:21:38 +0530

    checking fac_setails

--- beerf/beerf_15/static/app.js
+++ beerf/beerf_15/static/app.js
@@ -4,85 +4,100 @@ var app = angular.module('store',[]).config(function($interpolateProvider) {
     $interpolateProvider.endSymbol('$}');                      // So that django doesnt get confused
 });
 
-app.controller('storec',['fac_details', function(fac_details){					//controller for the data inside tabs
+//  CONTROLLERS
 
-this.products = [{
-	orders: [
-	{	
-		from:"R1",
-		order_no:100,
-		to_no:0,
-		transport:"none"
 
-	},
-	{	
-		from:"R2",
-		order_no:150,
-		to_no:0,
-		transport:"none"
 
-	},
-	{	
-		from:"R3",
-		order_no:200,
-		to_no:0,
-		transport:"none"
+app.controller('storec', ['fac_details', function(fac_details){					//controller for the data inside tabs
 
-	}
-	],
+	this.products = [{
+		orders: [
+		{	
+			from:"R1",
+			order_no:100,
+			to_no:0,
+			transport:"none"
 
-	inventory: [
-	{
-		day:0,
-		no_produced:0,
-		no_sold:0,
-		no_inv:0
-	},
-	{
-		day:1,
-		no_produced:0,
-		no_sold:0,
-		no_inv:0
-	}
-	],
+		},
+		{	
+			from:"R2",
+			order_no:150,
+			to_no:0,
+			transport:"none"
 
-	backlog:[
-	{
-		day:0,
-		no_produced:0,
-		no_ord:0,
-		no_to:0,
-		no_back:0
-	},
-	{
-		day:1,
-		no_produced:0,
-		no_ord:0,
-		no_to:0,
-		no_back:0
-	}
-	]
-}];
+		},
+		{	
+			from:"R3",
+			order_no:200,
+			to_no:0,
+			transport:"none"
 
-var vm = this;
+		}
+		],
 
-fac_details.success(function(json){
-	vm.factoryDetails = json;
-});
+		inventory: [
+		{
+			day:0,
+			no_produced:0,
+			no_sold:0,
+			no_inv:0
+		},
+		{
+			day:1,
+			no_produced:0,
+			no_sold:0,
+			no_inv:0
+		}
+		],
+
+		backlog:[
+		{
+			day:0,
+			no_produced:0,
+			no_ord:0,
+			no_to:0,
+			no_back:0
+		},
+		{
+			day:1,
+			no_produced:0,
+			no_ord:0,
+			no_to:0,
+			no_back:0
+		}
+		]
+	}];
+
+	var vm = this;
+
+	fac_details.success(function(json){
+		vm.factoryDetails = json;
+	});
 
 }]);
 
 
-app.controller('panelp',function(){                // controller for the panel
-this.tab=1;
-this.settab = function(a) { this.tab = a; };
-this.istab = function(a) { return this.tab === a;};
+app.controller('panelp',function(){         							       // controller for the panel
+	this.tab=1;
+	this.settab = function(a) { this.tab = a; };
+	this.istab = function(a) { return this.tab === a;};
 });
 
+
+
+
+//   SERVICES 
+
+
 app.factory('fac_details', ['$http', function($http){
-	return $http.get('http://127.0.0.1:8000/fac_details') 
-  				.success(function(data) {
-    					return data;
+
+	console.log('id from app.js', id);
+
+	return $http.post('http://127.0.0.1:8000/fac_details',
+				 {"user_id": id}
+				) 
+  				.success(function(json) {
+    					return json;
   					})
   				.error(function(err) {
     					return err;
